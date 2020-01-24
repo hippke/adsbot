9da3e87d4ddcb98ee386cd8aaab0559724e5ae1c
Michael Hippke <hippke@users.noreply.github.com>
2020-01-24 19:29:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,12 +54,12 @@ def check_if_new_citations(filename, query):
     new_paper_found = False
     search_string = "citations(" + query + ")"
     print('Running ADS with query', search_string)
-    try:
-        citing_papers = ads.SearchQuery(q=search_string, sort="date")  # , rows=20)
-        print('Query completed')
-    except:
-        print('Error in ADS query')
-        return new_paper_found
+    #try:
+    citing_papers = ads.SearchQuery(q=search_string, sort="date")  # , rows=20)
+    print('Query completed')
+    #except:
+    #    print('Error in ADS query')
+    #    return new_paper_found
     print('Query completed, after try..except block')
     for citing_paper in citing_papers:
         print(citing_paper.bibcode)
