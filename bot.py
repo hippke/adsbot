f9654aa7ea457d2c8069ed4d9279d406b5fce2ba
Michael Hippke <hippke@users.noreply.github.com>
2020-06-05 15:43:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ def ads_check(filename, query):
     papers.execute()
     print("Requests remaining:", papers.response.get_ratelimits()["remaining"])
     counter = 0
-    counter_max = int(papers.response.get_ratelimits()["remaining"]) - 5
+    counter_max = int(papers.response.get_ratelimits()["remaining"]) - 100
     new_cits = []
     for paper in papers:
         if paper.citation is not None:
