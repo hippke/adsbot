47a4ba614f670c783cc89dc8f6cb08e4e83b8720
hippke <michael@jaekle.info>
2021-03-25 16:49:20 +0000

    Add changes

--- mails/alexismssmith@gmail.com
+++
@@ -1,5 +0,0 @@
-New citation to:
-Skeletal mineralogy of bryozoans: Taxonomic and temporal patterns
-by: González-Delgado, Sara et al. - Chemical characterization of the Punta de Fuencaliente CO<SUB>2</SUB>-enriched system (La Palma, NE Atlantic Ocean): a new natural laboratory for ocean acidification studies
-https://ui.adsabs.harvard.edu/abs/2021BGeo...18.1673G
-
--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-Generalization of the Model of Magnetoelastic Effect: 3D Mechanical Stress Dependence of Magnetic Permeability Tensor in Soft Magnetic Materials
-by: Guo, Pingping et al. - Variable coefficient magnetic energy loss calculating model for magnetostrictive materials considering compressive stress
-https://ui.adsabs.harvard.edu/abs/2021AIPA...11c5227G
-
