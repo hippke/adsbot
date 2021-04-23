ba76dffa4288ac357164909e55a7ac6495e4eb45
hippke <michael@jaekle.info>
2021-04-23 13:02:50 +0000

    Add changes

--- mails/alexismssmith@gmail.com
+++
@@ -1,5 +0,0 @@
-New citation to:
-The physics of Lyman α escape from high-redshift galaxies
-by: Chen, Yuguang et al. - The KBSS-KCWI Survey: The connection between extended Ly$\alpha$ halos and galaxy azimuthal angle at $z\sim 2-3$
-https://ui.adsabs.harvard.edu/abs/2021arXiv210410173C
-
--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-Strain Dependence of Hysteretic Giant Magnetoimpedance Effect in Co-Based Amorphous Ribbon
-by: Gazda, Piotr et al. - Giant Stress-Impedance Effect in CoFeNiMoBSi Alloy in Variation of Applied Magnetic Field
-https://ui.adsabs.harvard.edu/abs/2021Mate...14.1919G
-
