998d4b98a1c7366008fcd36341eebd6c1047fa2a
hippke <michael@jaekle.info>
2021-09-03 08:08:37 +0000

    Add changes

--- mails/huanchen.hu94@gmail.com
+++
@@ -1,5 +0,0 @@
-New citation to:
-The International Pulsar Timing Array: second data release
-by: Bortolas, Elisa et al. - The competing effect of gas and stars in massive black hole binaries evolution
-https://ui.adsabs.harvard.edu/abs/2021arXiv210813436B
-
--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-Experimental Verification of Isotropic and Anisotropic Anhysteretic Magnetization Models
-by: Nowak, Paweł et al. - Inverse Transformation in Eddy Current Tomography with Continuous Optimization of Reference Defect Parameters
-https://ui.adsabs.harvard.edu/abs/2021Mate...14.4778N
-
