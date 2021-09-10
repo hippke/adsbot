98ce9b2b38bb1df115ba071cbe2638f47c08f37a
hippke <michael@jaekle.info>
2021-09-10 08:08:35 +0000

    Add changes

--- mails/dmontes@ucm.es
+++
@@ -1,5 +0,0 @@
-New citation to:
-Search for associations containing young stars (SACY). VII. New stellar and substellar candidate members in the young associations
-by: Zúñiga-Fernández, S. et al. - The HD 98800 quadruple pre-main sequence system. Towards full orbital characterisation using long-baseline infrared interferometry
-https://ui.adsabs.harvard.edu/abs/2021arXiv210902841Z
-
--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-Generalization of the Model of Magnetoelastic Effect: 3D Mechanical Stress Dependence of Magnetic Permeability Tensor in Soft Magnetic Materials
-by: Ostaszewska-Liżewska, Anna et al. - A FEM-Based Optimization Method for Driving Frequency of Contactless Magnetoelastic Torque Sensors in Steel Shafts
-https://ui.adsabs.harvard.edu/abs/2021Mate...14.4996O
-
