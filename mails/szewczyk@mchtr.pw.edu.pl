8390fa184685b28058240d01dd6509be265e5a61
hippke <michael@jaekle.info>
2022-04-30 08:11:30 +0000

    Add changes

--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-Experimental Verification of Isotropic and Anisotropic Anhysteretic Magnetization Models
-by: Szewczyk, Roman et al. - Improved gyrator-capacitor modeling of inductive components with a FINEMET-type nanocrystalline alloy core using SPICE
-https://ui.adsabs.harvard.edu/abs/2022JMMM..55569376S
-
