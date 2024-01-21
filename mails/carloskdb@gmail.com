863fc1272a86030fe52bcfbc113b4760bf17064b
hippke <michael@jaekle.info>
2024-01-21 10:15:47 +0000

    Add changes

--- mails/carloskdb@gmail.com
+++
@@ -1,5 +0,0 @@
-New citation to:
-The European Solar Telescope
-by: Díaz-Suárez, Sergio et al. - Numerical simulations of turbulence in prominence threads induced by torsional oscillations
-https://ui.adsabs.harvard.edu/abs/2024arXiv240109122D
-
--- mails/szewczyk@mchtr.pw.edu.pl
+++
@@ -1,5 +0,0 @@
-New citation to:
-A measuring setup for testing the mechanical stress dependence of magnetic properties of electrical steels
-by: Szumiata, Tadeusz et al. - The Two-Domain Model Utilizing the Effective Pinning Energy for Modeling the Strain-Dependent Magnetic Permeability in Anisotropic Grain-Oriented Electrical Steels
-https://ui.adsabs.harvard.edu/abs/2024Mate...17..369S
-
